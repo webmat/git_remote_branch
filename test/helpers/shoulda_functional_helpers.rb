278446f812510c375668bbabe0678bba91a23f92
Mathieu Martin <webmat@gmail.com>
2008-08-05 11:45:41 -0400

    Refactor the funk helpers to avoid some shoulda context nesting problems

--- test/helpers/shoulda_functional_helpers.rb
+++ test/helpers/shoulda_functional_helpers.rb
@@ -1,4 +1,5 @@
 module ShouldaFunctionalHelpers
+  include CaptureFu
   GRB_COMMAND = File.expand_path(File.dirname(__FILE__) + '/../../bin/grb') unless defined?(GRB_COMMAND)
   
   def self.included(base)
@@ -9,9 +10,24 @@ module ShouldaFunctionalHelpers
   end
   
   module InstanceMethods
-    def dir_stack
-      @dir_stack ||= DirStack.new
+    def current_dir
+      @current_dir || raise("@current_dir is not set. Warning, Will Robinson!")
     end
+    
+    def current_dir=(value)
+      @current_dir = value
+    end
+    
+    # Switches to one of the directories created by GitHelper:
+    #   :local1, :local2, :non_git or :remote
+    # This affects commands run with ``, system and so on.
+    def in_directory_for(dir)
+      # Just a reminder for my dumb head
+      raise "'in_directory_for' depends on @gh being set" unless @gh
+      
+      @current_dir = eval("@gh.#{dir}")
+    end
+    
     
     def run_with(params='')
       silencer = (params =~ /--silent/) ? '' : '--silent'
@@ -19,32 +35,39 @@ module ShouldaFunctionalHelpers
     end
 
     def execute(command)
-      `cd #{@dir_stack.current_dir} ; #{command}`
+      errno, returned_string = capture_process_output("cd #{current_dir} ; #{command}")
+      returned_string
     end
     
-    
-    def assert_branch(branch_name, branch_location)
-      case branch_location.to_sym
-      when :local
-        args = ''
-      when :remote
-        args = '-r'
-      else
-        raise ArgumentError, "Unknown branch location: #{branch_location.inspect}"
+    private
+      def get_branch_location(location)
+        case location.to_sym
+        when :local
+          args = '-l'
+        when :remote
+          args = '-r'
+        else
+          raise ArgumentError, "Unknown branch location: #{location.inspect}"
+        end
       end
-      
-      assert_match %r{#{branch_name}}, execute("git branch #{args}")
-    end
   end
 
   module ClassMethods
-    def should_create_branch(what_branch, where)
-      should "create branch '#{what_branch}' #{where == :local ? 'locally' : 'remotely'}" do
-        assert_branch what_branch, where
+    def should_have_branch(what_branch, where)
+      should "have the branch '#{what_branch}' #{where == :local ? 'locally' : 'remotely'}" do
+        args = get_branch_location(where)
+        assert_match(/#{what_branch}/, execute("git branch #{args}"))
       end
     end
     
-    def on_a_new_repo
+    def should_not_have_branch(what_branch, where)
+      should "not have the branch '#{what_branch}' #{where == :local ? 'locally' : 'remotely'}" do
+        args = get_branch_location(where)
+        assert_no_match(/#{what_branch}/, execute("git branch #{args}"))
+      end
+    end
+    
+    def on_a_repository
       context "on a new repository" do
         setup do
           @gh = GitHelper.new
@@ -58,22 +81,17 @@ module ShouldaFunctionalHelpers
       end
     end
     
-    # Switches to one of the directories created by GitHelper:
-    #   :local1, :local2, :non_git or :remote
-    # This affects commands run with ``, system and so on.
-    def in_directory_for(dir)
-      context "in directory for #{dir}" do
+    def in_non_git_directory
+      context "on a non-git related directory" do
         setup do
-          # Just a reminder for my dumb head
-          raise "'in_directory_for' depends on @gh being set" unless @gh
-          
-          dir_stack.pushd eval("@gh.#{dir}")
+          @temp_dir = TempDir.new
+          @current_dir = @temp_dir.directory
         end
         
         teardown do
-          dir_stack.popd
+          @temp_dir.cleanup
         end
-        
+
         yield
       end
     end
