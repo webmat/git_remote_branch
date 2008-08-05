a203e9037d88c1b4079ede94a2647442853d739b
Mathieu Martin <webmat@gmail.com>
2008-08-05 11:44:40 -0400

    Split the temp dir and temp git repos fucntionalities

--- test/helpers/git_helper.rb
+++ test/helpers/git_helper.rb
@@ -1,59 +1,33 @@
 require 'fileutils'
 require 'tmpdir'
+require File.dirname(__FILE__) + '/temp_dir_helper'
 
 # Instantiating a GitHelper object creates a temp directory containing 3 repos.
 # 1 that's considered the remote repo and 2 peer local repos (local1 and local2).
 # All 3 are synchronized with the same data (they contain a few dummy files).
 # Once instantiated you can access the 3 full repo locations through attribute readers
 # remote, local1 and local2.
-class GitHelper
-  include FileUtils
+class GitHelper < TempDirHelper
 
-  @@WORK_DIR = 'repo_test'
-  
-  attr_reader :remote, :local1, :local2, :non_git
+  attr_reader :remote, :local1, :local2
   
   def initialize
-    @common_directory = get_temp_dir
+    super('grb_test')
     
-    @remote  = init_repo(@common_directory, 'remote')
-    @local1  = clone_repo(@remote, @common_directory, 'local1')
-    @local2  = clone_repo(@remote, @common_directory, 'local2')
-    @non_git = File.join(@common_directory, 'non_git')
-  end
-  
-  def cleanup
-    rm_rf @common_directory
+    @remote  = init_repo(directory, 'remote')
+    @local1  = clone_repo(@remote, directory, 'local1')
+    @local2  = clone_repo(@remote, directory, 'local2')
   end
   
   protected
-  def get_temp_dir
-    #Note: it's NOT a good idea to do this stuff un a subdirectory of the 
-    #git_remote_branch repo. Trust me :-)
-    wd = File.expand_path( File.join( Dir::tmpdir, @@WORK_DIR) )
-    Dir.mkdir wd unless File.exists? wd
-    
-    #Create new subdir with a random name
-    new_dir=''
-    begin
-      new_dir = File.join( wd, "#{rand(10000)}" )
-      Dir.mkdir new_dir
-      Dir.mkdir File.join( new_dir, 'non_git' )
-    rescue
-      retry
+    def init_repo(path, name)
+      repo_dir = File.join(path, name)
+      `mkdir #{repo_dir}; cd $_; git init; touch file.txt; git add .; git commit -a -m "dummy file"`
+      repo_dir
     end
     
-    new_dir
-  end
-  
-  def init_repo(path, name)
-    repo_dir = File.join(path, name)
-    `mkdir #{repo_dir}; cd $_; git init; touch file.txt; git add .; git commit -a -m "dummy file"`
-    repo_dir
-  end
-  
-  def clone_repo(origin_path, clone_path, name)
-    `cd #{clone_path}; git clone #{File.join(origin_path, '.git')} #{name}`
-    return File.join(clone_path, name)
-  end
+    def clone_repo(origin_path, clone_path, name)
+      `cd #{clone_path}; git clone #{File.join(origin_path, '.git')} #{name}`
+      return File.join(clone_path, name)
+    end
 end
---
+++ test/helpers/temp_dir_helper.rb
@@ -0,0 +1,34 @@
+require 'fileutils'
+require 'tmpdir'
+
+class TempDirHelper
+  include FileUtils
+  
+  attr_reader :directory
+
+  def initialize(namespace='temp_dir_helper')
+    @directory = get_temp_dir!(namespace)
+  end
+  
+  def cleanup
+    rm_rf @directory
+  end
+  
+  private
+    def get_temp_dir!(namespace='')
+      wd = File.expand_path( File.join( Dir::tmpdir, namespace) )
+      mkdir wd unless File.exists? wd
+      
+      #Create new subdir with a random name
+      new_dir=''
+      begin
+        new_dir = File.join( wd, "#{rand(10000)}" )
+        mkdir new_dir
+        
+      rescue
+        retry
+      end
+      
+      new_dir
+    end
+end
