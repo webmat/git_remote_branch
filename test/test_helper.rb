7e4c4ef37f3c3a48e2c9ee90a14898e7efd1a0de
Mathieu Martin <webmat@gmail.com>
2008-11-10 15:28:16 -0500

    Make ruby-debug and redgreen optional when running tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,9 +10,12 @@ require 'mocha'
 
 # Just load redgreen if not running tests from TextMate
 IN_TM = !ENV['TM_DIRECTORY'].nil? unless defined?(IN_TM)
-require 'redgreen' unless IN_TM
-
-require 'ruby-debug'
+begin
+  require 'redgreen' unless IN_TM
+  require 'ruby-debug'
+rescue LoadError => ex
+  puts "Couldn't load optional test dependencies.\n  #{ex.inspect}"
+end
 
 require File.join( [test_dir] + %w{ .. lib git_remote_branch} )
 
