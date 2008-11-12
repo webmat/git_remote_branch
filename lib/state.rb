c2c3ec84997e08bb9f8f94d9683d88d9983b8199
Mathieu Martin <webmat@gmail.com>
2008-11-12 16:39:08 -0500

    Put the reading of the state in #local_branch_information...
    Now get_current_branch uses that.

--- lib/state.rb
+++ lib/state.rb
@@ -2,22 +2,34 @@ module GitRemoteBranch
   include ::CaptureFu
   
   private
-    BRANCH_LISTING_COMMAND = 'git branch -l'.freeze
+    LOCAL_BRANCH_LISTING_COMMAND = 'git branch -l'.freeze
   
   public
     def get_current_branch
+      local_branch_information[0]
+    end
+    
+    # Returns an array of 2 elements: [current_branch, [all local branches]]
+    def local_branch_information
       #This is sensitive to checkouts of branches specified with wrong case
       
-      listing = capture_process_output("#{BRANCH_LISTING_COMMAND}")[1]
+      listing = capture_process_output("#{LOCAL_BRANCH_LISTING_COMMAND}")[1]
+      
       raise(NotOnGitRepositoryError, listing.chomp) if listing =~ /Not a git repository/i
-      
-      current_branch = listing.scan(/^\*\s+(.+)/).flatten.first
-      
-      if current_branch =~ /\(no branch\)/
+      if listing =~ /\(no branch\)/
         raise InvalidBranchError, ["Couldn't identify the current local branch. The branch listing was:",
-          BRANCH_LISTING_COMMAND.red, 
+          LOCAL_BRANCH_LISTING_COMMAND.red, 
           listing].join("\n")
       end
-      current_branch.strip
+      
+      current_branch = nil
+      branches = listing.split("\n").map do |line| 
+        current        = line.include? '*'
+        clean_line     = line.gsub('*','').strip
+        current_branch = clean_line if current
+        clean_line
+      end
+      
+      return current_branch, branches
     end
 end
