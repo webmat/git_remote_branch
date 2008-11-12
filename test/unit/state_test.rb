55b47b8aec7f2363a2ff28da1e5c5b7fb0f10ead
Mathieu Martin <webmat@gmail.com>
2008-11-12 17:04:39 -0500

    Extract macro to detect correct crapping out in invalid situations

--- test/unit/state_test.rb
+++ test/unit/state_test.rb
@@ -4,7 +4,7 @@ require "#{TEST_DIR}/helpers/constants"
 class ParamReaderTest < Test::Unit::TestCase
   include ShouldaUnitHelpers
   
-  context 'get_current_branch' do
+  def self.craps_out_in_invalid_situations
     context "when not on a git repository" do
       setup do
         grb.stubs(:capture_process_output).returns([128, WHEN_NOT_ON_GIT_REPOSITORY])
@@ -14,7 +14,7 @@ class ParamReaderTest < Test::Unit::TestCase
         assert_raise(GitRemoteBranch::NotOnGitRepositoryError) { grb.get_current_branch }
       end
     end
-
+    
     context "when on an invalid branch" do
       setup do
         grb.stubs(:capture_process_output).returns([0, BRANCH_LISTING_WHEN_NOT_ON_BRANCH])
@@ -24,7 +24,11 @@ class ParamReaderTest < Test::Unit::TestCase
         assert_raise(GitRemoteBranch::InvalidBranchError) { grb.get_current_branch }
       end
     end
-
+  end
+  
+  context 'get_current_branch' do
+    craps_out_in_invalid_situations
+    
     context "when on a valid branch" do
       setup do
         grb.stubs(:capture_process_output).returns([0, REGULAR_BRANCH_LISTING])
